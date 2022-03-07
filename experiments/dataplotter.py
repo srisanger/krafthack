70be127ae39a24099a9e84c7925793b3ad13e915
Loeschenbrand <52955062+Loeschenbrand@users.noreply.github.com>
2022-03-07 11:05:42 +0100

    experimental data reader

--- experiments/dataplotter.py
+++ experiments/dataplotter.py
@@ -3,8 +3,17 @@ import matplotlib.pyplot as plt
 
 fileloc = 'C://Users//marku//Desktop//krafthack_files//'
 
-
-df2 = pd.read_parquet(fileloc+'input_dataset-2.parquet', engine='pyarrow')
 df1 = pd.read_parquet(fileloc+'input_dataset-1.parquet', engine='pyarrow')
+df2 = pd.read_parquet(fileloc+'input_dataset-2.parquet', engine='pyarrow')
+
+df_target = pd.read_parquet(fileloc+'prediction_input.parquet', engine='pyarrow')
+
+target_columns1 = set(df1.columns).intersection(df_target.columns)
+target_columns2 = set(df2.columns).intersection(df_target.columns)
+
+
+t1 = pd.date_range(df1.index[0],df1.index[-1], freq='1s')
+t2 = pd.date_range(df2.index[0],df2.index[-1], freq='1s') 
+
 
 print("fin")
