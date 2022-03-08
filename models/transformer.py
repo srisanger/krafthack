6ad1d8f9d2737bf305c5e08639b26be5e8b8cb60
Loeschenbrand <52955062+Loeschenbrand@users.noreply.github.com>
2022-03-08 09:05:54 +0100

    final submission

--- models/transformer.py
+++ models/transformer.py
@@ -5,10 +5,8 @@ import math
 
 class PositionalEncoding(nn.Module):
     # taken from https://pytorch.org/tutorials/beginner/transformer_tutorial.html
-    def __init__(self, d_model:int, dropout:float, max_len:int):
+    def __init__(self, d_model:int, max_len:int):
         super(PositionalEncoding, self).__init__()
-        self.dropout = nn.Dropout(p=dropout)
-
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)) # the formula from "Attention is all you need"
@@ -19,7 +17,7 @@ class PositionalEncoding(nn.Module):
 
     def forward(self, x):
         x_enc = x + self.pe[:x.size(0), :]
-        return self.dropout(x_enc)
+        return x_enc
 
 
 class Network(nn.Module):
@@ -28,10 +26,12 @@ class Network(nn.Module):
         assert (hidden_size%2 == 0) and (hidden_size%no_heads == 0)
         torch.cuda.empty_cache()
         self.inlayer = nn.Linear(input_size, hidden_size)
-        self.posenc = PositionalEncoding(hidden_size, dropout, maxlen)
-        self.encoder_layer = nn.TransformerEncoderLayer(d_model=hidden_size, nhead=no_heads, dim_feedforward=hidden_size, dropout=dropout)
+        self.posenc = PositionalEncoding(hidden_size, maxlen)
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=hidden_size, nhead=no_heads, dim_feedforward=hidden_size, dropout=0.)
         self.encoder = nn.TransformerEncoder(self.encoder_layer, num_layers)
         self.outlayer = nn.Linear(hidden_size, output_size)
+        # dropout is only added over the target layers:
+        self.dropout = torch.nn.Dropout(p=dropout, inplace=False)
         
         # move model to GPU:
         if torch.cuda.is_available():
@@ -40,6 +40,9 @@ class Network(nn.Module):
         
     def forward(self, x):
         # x input is batch, seq, features
+        # we apply the dropout only on the target layers:
+        x[:,:,self.outlayer.weight.shape[0]:] = self.dropout(x[:,:,self.outlayer.weight.shape[0]:])
+        # forward flow:
         h = self.inlayer(x)
         h = self.posenc(h.permute(1,0,2))
         h = self.encoder(h)[-1]
--- utils/visualizer.py
+++ utils/visualizer.py
@@ -4,35 +4,12 @@ import matplotlib.pylab as plt
 import numpy as np
 
 # plotting function:
-def trainingplot(tag, losshistory:list, predictionhistory:list, targethistory:list, periods:list, trainingtime:list, targets:list,
-                 plotlen:int=5000, figsize:tuple=(15,15)):
-    L = np.array(losshistory)
-    P = np.array(predictionhistory)
-    T = np.array(targethistory)
-    I = np.array(periods)
-    trainingtime = np.array(trainingtime)
-    
-    display.clear_output(wait=True)
-    fig, axs = plt.subplots(1+P.shape[1], figsize=figsize)
-    axs[0].plot(I, L, '-') 
-    avgloss = np.nanmean(L)
-    avgtime = np.nanmean(trainingtime) * 60 * 60 * 24
-    axs[0].title.set_text(f'loss - episode {tag} - average: {avgloss} | training time (seconds/day) - {avgtime}')
-    axs[0].grid()
-    
-    for i in range(P.shape[1]):
-        axs[1+i].plot(I[-plotlen:], T[:,i][-plotlen:], '-', label='outcome') 
-        axs[1+i].plot(I[-plotlen:], P[:,i][-plotlen:], '-', alpha=0.66, label='prediction') 
-        axs[1+i].title.set_text(targets[i])
-    plt.show()
-    
-# plotting function:
-def lossplot(tag, losshistory:list, plotlen:int=5000, figsize:tuple=(15,15)):
+def lossplot(tag, losshistory:list, plotlen:int=5000, figsize:tuple=(5,5)):
     L = np.array(losshistory)
     display.clear_output(wait=True)
     fig, axs = plt.subplots(1, figsize=figsize)
-    axs.plot(np.array(L)[-plotlen:], L[-plotlen:], '-') 
+    plt.plot(np.arange(len(L[-plotlen:])), L[-plotlen:], '-') 
     avgloss = np.nanmean(L)
-    axstitle.set_text(f'loss - episode {tag} - average: {avgloss}')
-    axs.grid()
+    plt.title(f"loss - episode {tag} - average: {format(avgloss, '.3g')}")
+    plt.grid()
     plt.show()
